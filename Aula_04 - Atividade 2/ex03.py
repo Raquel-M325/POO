e679462d040dd6f6e498313e33331e4d15bb5f4f
Raquel <raquelmartiniano14@gmail.com>
2025-09-24 15:01:16 +0000

    atualizações, resolvendo o exercicio

--- Aula_04 - Atividade 2/ex03.py
+++ Aula_04 - Atividade 2/ex03.py
@@ -1,4 +1,34 @@
 class Conta():     
     
-    def 
-    titular
+    def __init__(self):
+        self.titular = str(input())
+        self.numero = int(input())
+        self.saldo = float(input())
+
+    def saldo_antigo(self):
+        return self.saldo
+
+    def depositorio(self):
+        self.deposito = int(input('Digite o quanto você quer depositar: '))
+        return self.saldo + self.deposito
+
+    def saque(self):
+        self.sacar = int(input('Digite o quanto você quer sacar: '))
+        return self.saldo - self.sacar
+
+    def saldo_novo(self):
+        return saldo.
+
+#OBS: preciso saber é como atualizar o valor do saldo sem ficar separado
+
+classe = Conta() 
+
+print(f'Você depositou: {classe.depositorio()} R$')
+print(f'Você sacou: {classe.saque()} R$')
+print(f'Seu saldo anterior: {classe.saldo_antigo()} R$')
+print(f'Seu saldo atual: {classe.saldo.novo} R$')
+
+
+
+#saque é o que eu tiro, ou seja, eu subtraio com saldo
+#já o depositorio é o que acrescento o dinheiro, entao eu somo com o saldo
--- Aula_05/anotacoes.py
+++ Aula_05/anotacoes.py
@@ -1,2 +1,3 @@
 #entidade tem instancias, 
 # método de instãncia.self é a instância que chama o metodo
+#tem que usar o _init_(self) e colocar outros self para introduzir as variaveis, para depois fazer a operação
