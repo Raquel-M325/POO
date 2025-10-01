188ec9aee153acfd5d87d1a26a325a5462c5cafe
Raquel <raquelmartiniano14@gmail.com>
2025-10-01 13:30:43 +0000

    ajeitando

--- Aula_04 - Atividade 2/ex04.py
+++ Aula_04 - Atividade 2/ex04.py
@@ -2,6 +2,24 @@ class Cinema():
     def __init__(self):
         self.dia = str(input('dia: '))
         self.horario = int(input('hora: '))
+        self.valor = 0
+        self.listadehora= []
+        self.acrescimo = 0 
 
+    def noite(self):
+        for i in range(17, 24):
+            self.listadehora.append([i])
 
     def tempo(self):
+        if self.dia == "seg" or self.dia == "ter" or self.dia == "qui":
+            self.valor = 16
+            if self.horario == self.listadehora:
+                self.acrescimo = (50/100) * 16
+
+        elif self.dia == "sex" or self.dia == "sab" or self.dia == "dom":
+            self.valor = 20
+        elif self.dia == "qua":
+            self.valor == 8
+        
+
+        
