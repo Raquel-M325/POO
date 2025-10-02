3cb5db76a396f6318cf666cff4c7d7c3dda07a63
Raquel <raquelmartiniano14@gmail.com>
2025-10-02 20:48:50 +0000

    falta só resolver o erro da lista

--- Aula_04 - Atividade 2/ex04.py
+++ Aula_04 - Atividade 2/ex04.py
@@ -1,25 +1,49 @@
 class Cinema():
     def __init__(self):
-        self.dia = str(input('dia: '))
-        self.horario = int(input('hora: '))
+        self.dia = str(input('Qual dia para ver o filme, escolha e digite desse formato [seg, ter, qua, qui, sex, sab, dom]: '))
+        self.horario = int(input('Que horas: '))
         self.valor = 0
-        self.listadehora= []
         self.acrescimo = 0 
 
     def noite(self):
+        self.listadehora= []
         for i in range(17, 24):
-            self.listadehora.append([i])
+            self.listadehora.append(i)
+            return [i + 1]
+        return self.listadehora 
+        #Precisa ajeitar em relação das horas do desconto que ainda não está funcionando       
+    
 
     def tempo(self):
         if self.dia == "seg" or self.dia == "ter" or self.dia == "qui":
             self.valor = 16
-            if self.horario == self.listadehora:
-                self.acrescimo = (50/100) * 16
+            if self.horario == self.noite:
+                self.acrescimo = (50/100) * self.valor
+                return f'O ingresso custa {self.acrescimo:.2f} R$'
+            else:
+                return f'O ingresso custa {self.valor:.2f} R$'
+         
 
         elif self.dia == "sex" or self.dia == "sab" or self.dia == "dom":
             self.valor = 20
+            if self.horario == self.noite:
+                self.acrescimo = (50/100) * self.valor
+                return f'O ingresso custa {self.acrescimo:.2f} R$'
+            else:
+                return f'O ingresso custa {self.valor:.2f} R$'
+         
         elif self.dia == "qua":
-            self.valor == 8
-        
+            self.valor = 8
+            if self.horario == self.noite:
+                self.acrescimo = (50/100) * self.valor
+                return f'O ingresso custa {self.acrescimo:.2f} R$ de meia-entrada com acréscimo'
+            else:
+                return f'O ingresso custa {self.valor:.2f} R$ de meia-entrada'
+         
+    
+C = Cinema()
+
+print(C.tempo())
+
 
         
