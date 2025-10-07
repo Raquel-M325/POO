de07b9827842607290b618c40d40521e4fe35ffe
Raquel <raquelmartiniano14@gmail.com>
2025-10-07 02:27:24 +0000

    mais um exercicio concluido

--- Aula_06 - Atividade 3/ex04.py
+++ Aula_06 - Atividade 3/ex04.py
@@ -6,12 +6,14 @@ class Cinema():
     def set_ingresso(self, i):
         if i < 0:
             print('Valor Inválido')
-        return i == self.ingresso
+        else:
+            self.ingresso = i
 
     def set_acrescimo(self, a):
         if a < 0:
             print('Valor Inválido')
-        return a == self.acrescimo
+        else:
+            self.acrescimo = a
 
     def get_ingresso(self):
         return self.ingresso
@@ -20,6 +22,38 @@ class Cinema():
         return self.acrescimo
 
     def calc_agendamento(self):
-        #ainda estou fazendo
+        self.acrescimo = 0.5 * self.ingresso
+        return self.ingresso + self.acrescimo
+
 
 c = Cinema()
+
+#----------------INTERFACE DO USUÁRIO----------------------- 
+noite = []
+
+for i in range(17, 25):
+    noite.append(i)
+    
+dias = str(input('Qual dia você quer ver o filme, seguindo nesse formato [seg, ter, qua, qui, sex, sab, dom]: '))
+horario = int(input('Qual horário: '))
+if dias == 'sex' or dias == 'sab' or dias == 'dom':
+    c.set_ingresso(18)
+    if horario in noite:
+        print(f'Ingresso com acréscimo: R$ {c.calc_agendamento():.2f}')
+    else:
+        print(f'Ingresso: R$ {c.get_ingresso():.2f}')
+
+
+elif dias == 'seg' or dias == 'ter' or dias == 'qui':
+    c.set_ingresso(16)
+    if horario in noite:
+        print(f'Ingresso com acréscimo: R$ {c.calc_agendamento():.2f}')
+    else:
+        print(f'Ingresso: R$ {c.get_ingresso():.2f}')
+
+
+elif dias == 'qua':
+    c.set_ingresso(8)
+    print(f'Ingresso de meia entrada: R$ {c.get_ingresso():.2f}, lembrando que não tem acréscimo nas quartas!')
+
+
