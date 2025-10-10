3823917590aa1c1553d5222dfa956053f13279df
Raquel <raquelmartiniano14@gmail.com>
2025-10-10 13:03:47 +0000

    estudando

--- Aula_07 - Atividade 4/ex03.py
+++ Aula_07 - Atividade 4/ex03.py
@@ -21,6 +21,7 @@ class Conversor:
     
     def Binario(self):
 
+
     def __str__(self): #ToString
         return f'Número Binário: {}/n Número Decimal: {self.num}'
 
@@ -28,3 +29,35 @@ class Conversor:
 c = Conversor(5)
 print(c.ToString())
         
+
+# class Conversor:
+#     def __init__(self, num):
+#         self.set_num(num) 
+
+#     def set_num(self, num):
+#         self.__num = num
+#     def get_num(self):
+#         return self.__num
+    
+#     def binario(self):
+#         restos = []              # pilha
+#         n = self.__num
+#         while n > 0:
+#             restos.append(n % 2) # push - coloca na pilha
+#             n = n // 2
+#         bin = ""
+#         while len(restos) > 0:
+#             bin = bin + str(restos.pop()) # pop - retira da pilha
+#         return bin
+#     def binario2(self):
+#         bin = ""
+#         n = self.__num
+#         while n > 0:
+#             bin = str(n % 2) + bin 
+#             n = n // 2            
+#         return bin
+#     def __str__(self):
+#         return f"Decimal = {self.__num}, Binário = {self.binario()} {self.binario2()}"
+
+# x = Conversor(14)
+# print(x)
